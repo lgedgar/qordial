310f236c44c1898c9de879f8990e6950092aae0a
Lance Edgar <lance@edbob.org>
2024-01-21 12:12:30 -0600

    Re-structure as library package; add SelectPicker component
    just to make sure things work correctly at a base level

---
+++ lib/components/ServicePicker.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+
+    props: {
+        modelValue: null,
+        required: {
+            type: Boolean,
+        },
+        includeServices: {
+            type: Array,
+        },
+    },
+
+    emits: ['update:modelValue'],
+
+    computed: {
+
+        availableServices() {
+            return this.includeServices || []
+        },
+
+        value: {
+            get() {
+                return this.modelValue
+            },
+            set(value) {
+                this.$emit('update:modelValue', value)
+            },
+        },
+    },
+}
+</script>
+
+<template>
+  <o-select v-model="value">
+    <option v-if="!required" :value="null">(any)</option>
+    <option v-for="svc in availableServices"
+            :key="svc"
+            :value="svc">
+      {{ svc }}
+    </option>
+  </o-select>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,40 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  // nb. much of this was inspired by
+  // https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      //entry: resolve(__dirname, 'lib/main.js'),
+      entry: resolve(__dirname, 'lib/index.js'),
+      name: 'qordial',
+      // the proper extensions will be added
+      fileName: 'qordial',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [
+          'pinia',
+          'vue',
+      ],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+
   plugins: [
     vue(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
 })
