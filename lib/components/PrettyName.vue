446e6a2a9c50a4c22dbe850d146e98074ac55410
Lance Edgar <lance@edbob.org>
2024-04-18 21:58:00 -0500

    Add the PrettyName component
    shows a Qortal username as hyperlink, clicking opens profile modal

---
+++ lib/components/PrettyName.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+
+    props: {
+        value: {
+            required: true,
+        },
+    },
+
+    methods: {
+        async showProfile() {
+            await qortalRequest({
+                action: 'OPEN_PROFILE',
+                name: this.value,
+            })
+        },
+    },
+}
+</script>
+
+<template>
+  <a href="#" @click.prevent="showProfile()">{{ value }}</a>
+</template>
